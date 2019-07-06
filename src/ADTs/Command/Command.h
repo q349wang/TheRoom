f051c9e8fa14c152d84e502a714fc43dd0e3b544
Leo Wang <q349wang@edu.uwaterloo.ca>
2019-07-06 19:23:33 -0400

    continued implementation of battlemanager

--- src/ADTs/Command/Command.h
+++ src/ADTs/Command/Command.h
@@ -1,12 +1,17 @@
+#ifndef COMMAND_H_
+#define COMMAND_H_
+
 #include <vector>
+#include <string>
 
 class Command {
     char cmd;
-    std::vector<char> args;
+    std::vector<std::string> args;
     public:
     Command(char);
-    Command(char, std::vector<char>);
+    Command(char, std::vector<std::string>);
 
     const char& getCommand() const;
-    const std::vector<char>& getArgs() const;
+    const std::vector<std::string>& getArgs() const;
 };
+#endif
--- src/Managers/BattleManager.h
+++ src/Managers/BattleManager.h
@@ -1,10 +1,33 @@
+#ifndef BATTLEMANAGER_H_
+#define BATTLEMANAGER_H_
+
+#include <vector>
+#include <string>
+
 #include "../HelperClasses/Subject.h"
 
+class Command;
+class Enemy;
+class Player;
+
 class BattleManager : public Subject {
+    std::vector<Enemy*>* eList;
+    Player* player;
+
+    bool battleEnded;
+    std::string msg;
+    int eLeft;
     public:
-    BattleManager();
+    BattleManager(Player *);
     ~BattleManager();
 
     void runEnemyTurn();
-    void 
+    bool runPlayerTurn(const Command&);
+    void startBattle(std::vector<Enemy*>*);
+    void runBattle();
+    void endBattle();
+
+    bool isBattleEnded();
+    std::string getMsg();
 };
+#endif
