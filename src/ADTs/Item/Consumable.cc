68876138ae3335e058106c41866cd9a8aad2e98b
Anish Pangarkar <apangark@uwaterloo.ca>
2019-07-11 16:31:13 -0400

    made the item classe abstract, added a setter.

---
+++ src/ADTs/Item/Consumable.cc
@@ -0,0 +1,12 @@
+#include "Consumable.h"
+#include <utility>
+#include <string>
+
+using namespace std;
+
+Consumable::Consumable(string _name){
+    setString(_name);
+}
+Consumable::~Consumable(){
+    
+}
