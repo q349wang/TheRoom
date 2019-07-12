32804d40580c0f2317559421c4d7ff5ba68ce6a7
Leo Wang <q349wang@edu.uwaterloo.ca>
2019-07-12 02:33:11 -0400

    forked a2q2extra Xwindow code

--- src/Managers/XWindowManager.cc
+++ src/Managers/XWindowManager.cc
@@ -1,16 +1,80 @@
 #include "XWindowManager.h"
-using namespce std;
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
 
-XWindowManager::XWindowManager() {
-    /* open the connection to the display "simey:0". */
-    d = XOpenDisplay(NULL);
-    if (d == NULL) {
-        fprintf(stderr, "Cannot connect to X server %s\n", "simey:0");
-        exit(-1);
-    }:
-}
+using namespace std;
 
 XWindowManager::~XWindowManager() {
     XFreeGC(d, gc);
     XCloseDisplay(d);
 }
+
+XWindowManager::XWindowManager(int width, int height) {
+	d = XOpenDisplay(NULL);
+	if (d == NULL) {
+		cerr << "Cannot open display" << endl;
+		exit(1);
+	}
+	
+	s = DefaultScreen(d);
+	w = XCreateSimpleWindow(
+		d, RootWindow(d, s), 10, 10, width, height, 
+		1, BlackPixel(d, s), WhitePixel(d, s)
+	);
+
+	XSelectInput(d, w, ExposureMask | KeyPressMask);
+
+	Pixmap pix = XCreatePixmap(
+		d, w, width, height, 
+		DefaultDepth(d, DefaultScreen(d))
+	);
+
+	gc = XCreateGC(d, pix, 0, 0);
+
+	// Set up colours.
+	XColor xcolour;
+	Colormap cmap;
+
+	const size_t numColours = 5;
+	char color_vals[numColours][10] = {
+		"white", "black", "red", 
+		"green", "blue"
+	};
+
+	cmap = DefaultColormap(d, DefaultScreen(d));
+	
+	for(unsigned int i = 0; i < numColours; ++i) {
+		XParseColor(d, cmap, color_vals[i], &xcolour);
+		XAllocColor(d, cmap, &xcolour);
+		colours[i] = xcolour.pixel;
+	}
+
+	XSetForeground(d, gc, colours[Black]);
+
+	 // Make window non-resizeable.
+	XSizeHints hints;
+	hints.flags = (USPosition | PSize | PMinSize | PMaxSize );
+	hints.height = hints.base_height = hints.min_height = hints.max_height = height;
+	hints.width = hints.base_width = hints.min_width = hints.max_width = width;
+	XSetNormalHints(d, w, &hints);
+
+	// map window and flush
+	XMapRaised(d, w);
+	XFlush(d);
+	
+	// wait 1 second for setup
+	sleep(1);
+}
+void XWindowManager::drawFillRect(int x, int y, int width, int height, int colour) {
+	XSetForeground(d, gc, colours[colour]);
+	XFillRectangle(d, w, gc, x, y, width, height);
+	XFlush(d);
+}
+
+void XWindowManager::drawString(int x, int y, string msg) {
+	XDrawString(d, w, DefaultGC(d, s), x, y, msg.c_str(), msg.length());
+	XFlush(d);
+}
+
--- src/Managers/XWindowManager.h
+++ src/Managers/XWindowManager.h
@@ -1,11 +1,23 @@
 #include <X11/Xlib.h> /* defines common Xlib functions and structs. */
-
+#include <X11/Xutil.h>
 class XWindowManager {
     /* this variable will contain the pointer to the Display structure */
     /* returned when opening a connection.                             */
     Display* d;
+    Window w;
+    int s;
+    GC gc;
+
+    unsigned long colours[10];
 
    public:
-    XWindowManager();
+    XWindowManager(int width = 500, int height = 500);
     ~XWindowManager();
-}
+    XWindowManager(const XWindowManager&) = delete;
+    XWindowManager& operator=(const XWindowManager&) = delete;
+
+    enum {White=0, Black, Red, Green, Blue};
+
+    void drawFillRect(int x, int y, int w, int h, int col);
+    void drawString(int x, int y, std::string msg);
+};
