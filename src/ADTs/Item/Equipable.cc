8dcc56b7ecec9f03b5b5480f841bba29cb59e9ee
Leo Wang <q349wang@edu.uwaterloo.ca>
2019-07-16 00:55:01 -0400

    added durability

--- src/ADTs/Item/Equipable.cc
+++ src/ADTs/Item/Equipable.cc
@@ -1,10 +1,28 @@
 #include "Equipable.h"
+#include "BaseDescription.h"
 
 using namespace std;
 
 map<string, StatMod> Equipable::getPassive() const { return passive; }
 
 Equipable::Equipable(string name, shared_ptr<ItemDescription> active,
-                     map<string, StatMod>  passive)
-    : Item{name, active}, passive{passive} {}
+                     map<string, StatMod> passive, int durability)
+    : Item{name, active}, passive{passive}, durability{durability} {}
 Equipable::~Equipable() {}
+
+// Decrement durability after use, (-1 is infinite)
+void Equipable::afterUse()
+{
+    if (durability <= -1)
+    {
+        return;
+    }
+    else if (durability == 0)
+    {
+        desc = make_shared<BaseDescription>(getName());
+    }
+    else
+    {
+        durability--;
+    }
+}
