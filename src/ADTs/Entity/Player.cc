27a59f21a65e60d9ec8bf44b013f7cf6392bbdb9
Leo Wang <q349wang@edu.uwaterloo.ca>
2019-07-18 14:06:30 -0400

    added equip

--- src/ADTs/Entity/Player.cc
+++ src/ADTs/Entity/Player.cc
@@ -7,7 +7,6 @@
 #include <utility>
 #include <iostream>
 
-
 using namespace std;
 
 /**
@@ -15,11 +14,10 @@ using namespace std;
  *                   vector<shared_ptr<Consumable>>, vector<shared_ptr<Equipable>>)
  * Purpose: Constructor which requires initial player's health, energy, and position
  */
-Player::Player(double health, double energy, double attack, double armour, 
-               string name, pair<int, int> position, 
-               vector<shared_ptr<Consumable>> consumables, 
-               vector<shared_ptr<Equipable>> equipables) :
-               Entity{health, energy, attack, armour, name, position, consumables, equipables} {}
+Player::Player(double health, double energy, double attack, double armour,
+               string name, pair<int, int> position,
+               vector<shared_ptr<Consumable>> consumables,
+               vector<shared_ptr<Equipable>> equipables) : Entity{health, energy, attack, armour, name, position, consumables, equipables} {}
 
 /**
  * Signature: ~Entity()
@@ -31,8 +29,10 @@ Player::~Player() {}
  * Signature: void decreaseCooldown()
  * Purpose: Decreases the cooldown after every specified turn
  */
-void Player::decreaseCooldown() {
-    if(cooldown_ > BASE_SPECIAL_READY) {
+void Player::decreaseCooldown()
+{
+    if (cooldown_ > BASE_SPECIAL_READY)
+    {
         cooldown_--;
     }
 }
@@ -41,7 +41,8 @@ void Player::decreaseCooldown() {
  * Signature: void resetCooldown()
  * Purpose: Resets the cooldown of a Player's special movement
  */
-void Player::resetCooldown() {
+void Player::resetCooldown()
+{
     cooldown_ = BASE_SPECIAL_COOLDOWN;
 }
 
@@ -49,7 +50,8 @@ void Player::resetCooldown() {
  * Signature: specialReady()
  * Purpose: Determines if Player's special movement is a
  */
-bool Player::specialReady() {
+bool Player::specialReady()
+{
     return (cooldown_ == BASE_SPECIAL_READY);
 }
 
@@ -57,7 +59,8 @@ bool Player::specialReady() {
  * Signature: int getCooldown()
  * Purpose: Provides the current special movement cooldown
  */
-int Player::getCooldown() {
+int Player::getCooldown()
+{
     return cooldown_;
 }
 
@@ -66,35 +69,40 @@ int Player::getCooldown() {
  * Purpose: Determines if a provided move is possible for a player
  *          Utilizes 'N', 'E', 'S', 'W' for direction indication
  */
-bool Player::checkMove(char direction) {
+bool Player::checkMove(char direction)
+{
     pair<int, int> updated_position = position_;
 
     // Update the modified position dependent on the input direction
-    switch(direction) {
-        case 'E':
-            updated_position.first++;
-            break;
+    switch (direction)
+    {
+    case 'E':
+        updated_position.first++;
+        break;
 
-        case 'W':
-            updated_position.first--;
-            break;
+    case 'W':
+        updated_position.first--;
+        break;
 
-        case 'N':
-            updated_position.second--;
-            break;
+    case 'N':
+        updated_position.second--;
+        break;
 
-        case 'S':
-            updated_position.second++;
-            break;
+    case 'S':
+        updated_position.second++;
+        break;
 
-        default:
-            return false;
+    default:
+        return false;
     }
 
-    if((updated_position.second >= 0)  && (updated_position.second < current_map_->numRows())) {
-        if((updated_position.first >= 0) && (updated_position.first < current_map_->numColumns(updated_position.second))) {    
-            if((current_map_->tile(updated_position.first, updated_position.second)).available()) {
-                    return true;
+    if ((updated_position.second >= 0) && (updated_position.second < current_map_->numRows()))
+    {
+        if ((updated_position.first >= 0) && (updated_position.first < current_map_->numColumns(updated_position.second)))
+        {
+            if ((current_map_->tile(updated_position.first, updated_position.second)).available())
+            {
+                return true;
             }
         }
     }
@@ -107,32 +115,35 @@ bool Player::checkMove(char direction) {
  * Purpose: Makes a move in a specified direction (indicated using 'N', 'E', 'S', 'W')
  *          If move was valid, returns true, otherwise remains stationary and returns false
  */
-bool Player::makeMove(char direction) {
+bool Player::makeMove(char direction)
+{
     pair<int, int> updated_position = position_;
-    
-    switch(direction) {
-        case 'E':
-            updated_position.first++;
-            break;
 
-        case 'W':
-            updated_position.first--;
-            break;
+    switch (direction)
+    {
+    case 'E':
+        updated_position.first++;
+        break;
 
-        case 'N':
-            updated_position.second--;
-            break;
+    case 'W':
+        updated_position.first--;
+        break;
 
-        case 'S':
-            updated_position.second++;
-            break;
+    case 'N':
+        updated_position.second--;
+        break;
 
-        default:
-            // Return false if any other input is detected!
-            return false;
+    case 'S':
+        updated_position.second++;
+        break;
+
+    default:
+        // Return false if any other input is detected!
+        return false;
     }
-    
-    if(checkMove(direction)) {
+
+    if (checkMove(direction))
+    {
         updatePosition(updated_position);
         return true;
     }
@@ -144,13 +155,17 @@ bool Player::makeMove(char direction) {
  * Signature: void consumeConsumable(shared_ptr<Consumable>);
  * Purpose: Utilizes a specified consumable item
  */
-void Player::consumeConsumable(shared_ptr<Entity> entity, string consume_name) {
+void Player::consumeConsumable(shared_ptr<Entity> entity, string consume_name)
+{
     vector<shared_ptr<Consumable>> consumables = (entity->currentConsumables());
 
-    for(auto existing = consumables.begin(); existing != consumables.end(); ++existing) {
-        if(consume_name == (*existing)->getName()) {
+    for (auto existing = consumables.begin(); existing != consumables.end(); ++existing)
+    {
+        if (consume_name == (*existing)->getName())
+        {
             map<string, StatMod> consume_mods = (*existing)->useItem();
-            for(auto it = consume_mods.begin(); it != consume_mods.end(); ++it) {
+            for (auto it = consume_mods.begin(); it != consume_mods.end(); ++it)
+            {
                 entity->applyStat((*it).first, (*it).second);
             }
             consumables.erase(existing);
@@ -163,14 +178,19 @@ void Player::consumeConsumable(shared_ptr<Entity> entity, string consume_name) {
  * Signature: void equipEquipable(shared_ptr<Equipable>);
  * Purpose: Utilizes a specified equipable item
  */
-void Player::equipEquipable(shared_ptr<Entity> entity, string equip_name) {
+void Player::equipEquipable(shared_ptr<Entity> entity, string equip_name)
+{
     vector<shared_ptr<Equipable>> equipables = (entity->currentEquipables());
-    for(auto existing = equipables.begin(); existing != equipables.end(); ++existing) {
-        if(equip_name == (*existing)->getName()) {
+    for (auto existing = equipables.begin(); existing != equipables.end(); ++existing)
+    {
+        if (equip_name == (*existing)->getName())
+        {
 
-            if((*existing)->getDurability() > 0) {
+            if ((*existing)->getDurability() > 0)
+            {
                 map<string, StatMod> equip_mods = (*existing)->useItem();
-                for(auto it = equip_mods.begin(); it != equip_mods.end(); ++it) {
+                for (auto it = equip_mods.begin(); it != equip_mods.end(); ++it)
+                {
                     entity->applyStat((*it).first, (*it).second);
                 }
             }
@@ -184,12 +204,16 @@ void Player::equipEquipable(shared_ptr<Entity> entity, string equip_name) {
  * Signature: void dropEquipable(string)
  * Purpose: Drops a specified equipable item
  */
-void Player::dropEquipable(string equip_name) {
-    for(auto existing = equipables_.begin(); existing != equipables_.end(); ++existing) {
-        if(equip_name == (*existing)->getName()) {
+void Player::dropEquipable(string equip_name)
+{
+    for (auto existing = equipables_.begin(); existing != equipables_.end(); ++existing)
+    {
+        if (equip_name == (*existing)->getName())
+        {
 
             map<string, StatMod> equip_mods = (*existing)->getPassive();
-            for(auto it = equip_mods.begin(); it != equip_mods.end(); ++it) {
+            for (auto it = equip_mods.begin(); it != equip_mods.end(); ++it)
+            {
                 reverseStat((*it).first, (*it).second);
             }
             equipables_.erase(existing);
@@ -202,34 +226,44 @@ void Player::dropEquipable(string equip_name) {
  * Signature: void reverseStat(string, StatMod)
  * Purpose: Reverses the stat mods implications for a player
  */
-void Player::reverseStat(string stat, StatMod mod) {
-    if(mod.getMultiplier() != 0) {
-        if((stat == "Ranger") || (stat == "Mage") || (stat == "Warrior")) {
-            if(stat == getName()) {
-                armour_ = ((armour_)/mod.getMultiplier()) - mod.getAdder();
-                health_ = ((health_)/mod.getMultiplier()) - mod.getAdder();
-                energy_ = ((energy_)/mod.getMultiplier()) - mod.getAdder();
-                attackStrength_ = ((attackStrength_)/mod.getMultiplier()) - mod.getAdder();
+void Player::reverseStat(string stat, StatMod mod)
+{
+    if (mod.getMultiplier() != 0)
+    {
+        if ((stat == "Ranger") || (stat == "Mage") || (stat == "Warrior"))
+        {
+            if (stat == getName())
+            {
+                armour_ = ((armour_) / mod.getMultiplier()) - mod.getAdder();
+                health_ = ((health_) / mod.getMultiplier()) - mod.getAdder();
+                energy_ = ((energy_) / mod.getMultiplier()) - mod.getAdder();
+                attackStrength_ = ((attackStrength_) / mod.getMultiplier()) - mod.getAdder();
             }
         }
 
-        else {
-            if(stat == "Health") {
-                health_ = ((health_)/mod.getMultiplier()) - mod.getAdder();
+        else
+        {
+            if (stat == "Health")
+            {
+                health_ = ((health_) / mod.getMultiplier()) - mod.getAdder();
             }
-            else if(stat == "Attack") {
-                attackStrength_ = ((attackStrength_)/mod.getMultiplier()) - mod.getAdder();
+            else if (stat == "Attack")
+            {
+                attackStrength_ = ((attackStrength_) / mod.getMultiplier()) - mod.getAdder();
             }
-            else if(stat == "Damage") {
-                health_ = ((health_)/mod.getMultiplier()) + mod.getAdder();
+            else if (stat == "Damage")
+            {
+                health_ = ((health_) / mod.getMultiplier()) + mod.getAdder();
             }
-            else if(stat == "Energy") {
-                energy_ = ((energy_ )/mod.getMultiplier()) - mod.getAdder();
-            }   
-            else if(stat == "Armor") {
-                armour_ = ((armour_)/mod.getMultiplier()) - mod.getAdder();
+            else if (stat == "Energy")
+            {
+                energy_ = ((energy_) / mod.getMultiplier()) - mod.getAdder();
             }
-        }   
+            else if (stat == "Armor")
+            {
+                armour_ = ((armour_) / mod.getMultiplier()) - mod.getAdder();
+            }
+        }
     }
 }
 
@@ -237,9 +271,11 @@ void Player::reverseStat(string stat, StatMod mod) {
  * Signature: void addEquipable(shared_ptr<Equipable>)
  * Purpose: Equips equipable and activates passive stat mods
  */
-void Player::addEquipable(shared_ptr<Equipable> equip) {
+void Player::addEquipable(shared_ptr<Equipable> equip)
+{
     map<string, StatMod> passive_mods = equip->getPassive();
-    for(auto it = passive_mods.begin(); it != passive_mods.end(); ++it) {
+    for (auto it = passive_mods.begin(); it != passive_mods.end(); ++it)
+    {
         applyStat((*it).first, (*it).second);
     }
 
@@ -250,6 +286,21 @@ void Player::addEquipable(shared_ptr<Equipable> equip) {
  * Signature: void pickUpItems()
  * Purpose: Picks up all items stored on its current tile, and empties tile
  */
-vector<shared_ptr<Item>> Player::pickUpItems() {
-    return current_map_->pickUpItems(position_.first, position_.second);
+vector<shared_ptr<Item>> Player::pickUpItems()
+{
+
+    vector<shared_ptr<Item>> pickedUp = current_map_->pickUpItems(position_.first, position_.second);
+    for (auto pick : pickedUp)
+    {
+        if (pick->getType() == 0)
+        {
+            addConsumable(static_pointer_cast<Consumable>(pick));
+        }
+        else
+        {
+            addEquipable(static_pointer_cast<Equipable>(pick));
+        }
+    }
+
+    return pickedUp;
 }
